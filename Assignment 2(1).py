f9ff69434d5c9de0c4cafbae15c1e265e054f876
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2021-10-24 16:15:25 +0800

    Third commit

---
+++ Assignment 2(1).py
@@ -0,0 +1,7 @@
+apples = int(input("How many apples? "))
+orange = int(input("How many oranges? "))
+apple_price = 20
+orange_price = 25
+total_cost = (apples * apple_price) + (orange * orange_price) 
+print(f"The total amount is {total_cost}.") 
+
