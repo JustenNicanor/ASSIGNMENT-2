54f88ef91a847732a640003573aee8ad198f3c16
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2021-10-24 13:27:37 +0800

    Second commit

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,4 +1,4 @@
 name = input("What is your name? ")
 age = input("What is your age? ")
 address = input("what is your address? ")
-print("Hi, my name is", name, "I am", age, "years old and I live in", address, )
+print(f"Hi, my name is {name}. I am {age} years old and I live in {address}")
